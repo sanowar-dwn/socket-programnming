340af0c211afe8c061de23e260e290d3668fe37a
Student's <sanowardwnofficial@gmail.com>
2022-09-16 20:07:32 +0600

    first

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+HOST = '192.168.199.1'
+PORT = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((HOST, PORT))
+
+server.listen(5)
+
+while True:
+    communictaion_socket, address = server.accept()
+    print(f'connected to {address}')
+    message = communictaion_socket.recv(1024).decdoe('utf-8')
+    print(f'message from the client {message}')
+    communictaion_socket.send('got your message'.encode('utf-8'))
+    communictaion_socket.close()
+
+    print(f"connection with {address} has ended")
+
+
